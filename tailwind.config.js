b02fb71ba3032aa2337291531d1bda4571dd6859
EXREY1 <andrejkajdalov51676@gmail.com>
2025-04-22 13:30:40 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  module.exports = {
+    theme: {
+      extend: {
+        ringWidth: {
+          '10': '10px',
+        }
+      }
+    }
+  }
